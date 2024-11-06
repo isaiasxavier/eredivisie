965d118df5be944bdb5f82ca8d94b22e53faa4d6
Isaias Xavier Santana <isaiasxavier@outlook.com>
2024-11-06 10:45:17 +0100

    libs.retrofit and libs.converter.gson

---
+++ app/src/main/java/com/football/eredivisie/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.football.eredivisie.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
