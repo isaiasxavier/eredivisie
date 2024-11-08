e186920661a907797e8a4aebbdd1c4b0d7cf0865
Isaias Xavier Santana <isaiasxavier@outlook.com>
2024-11-08 13:32:56 +0100

    feat homeFragment redirecting to Standings

--- app/src/main/java/com/football/eredivisie/ui/home/HomeFragment.kt
+++ app/src/main/java/com/football/eredivisie/ui/home/HomeFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.football.eredivisie.R
 import androidx.fragment.app.Fragment
 import com.football.eredivisie.databinding.FragmentHomeBinding
+import com.football.eredivisie.ui.standings.StandingsFragment
 
 class HomeFragment : Fragment() {
 
@@ -22,6 +24,17 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.standingsHome.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment_content_main, StandingsFragment())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
     override fun onDestroyView() {
