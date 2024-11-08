3ef852a7b8b4da7c36426b01a850ceb755172d6a
Isaias Xavier Santana <isaiasxavier@outlook.com>
2024-11-08 15:49:52 +0100

    feat Home v2

--- app/src/main/java/com/football/eredivisie/ui/home/HomeFragment.kt
+++ app/src/main/java/com/football/eredivisie/ui/home/HomeFragment.kt
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.football.eredivisie.R
 import com.football.eredivisie.databinding.FragmentHomeBinding
+import com.football.eredivisie.ui.matches.MatchesFragment
 import com.football.eredivisie.ui.standings.StandingsFragment
+import com.football.eredivisie.ui.teams.TeamsFragment
 
 class HomeFragment : Fragment() {
 
@@ -31,13 +33,43 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Carregar a imagem usando Glide
+        // Standings
         Glide.with(this)
             .load(R.drawable.standings_home_png)
             .into(binding.standingsHome)
 
+        // Matches
+        Glide.with(this)
+            .load(R.drawable.matches_home_png)
+            .into(binding.matchesHome)
+
+        // Teams
+        Glide.with(this)
+            .load(R.drawable.team_home)
+            .into(binding.teamsHome)
+
+        // Profile
+        Glide.with(this)
+            .load(R.drawable.profile_home)
+            .into(binding.profileHome)
+
         binding.standingsHome.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.nav_host_fragment_content_main, StandingsFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        binding.matchesHome.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment_content_main, MatchesFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        binding.teamsHome.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment_content_main, TeamsFragment())
                 .addToBackStack(null)
                 .commit()
         }
